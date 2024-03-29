725684fde59f413e63f07e4a03aa756e6b200ba9
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-03-29 17:42:54 +0700

    jenkinsfile

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -13,7 +13,7 @@ TIME="<i>$(date +%T)</i>"
 
 # Text message
 # $BUILD_NUMBER - Set for current build number "e.g, 1,2,3"
-MESSAGE="<b>🚀DEPLOYMENT STATUS</b>: $1%0A%0A<b>🔁VERSION</b>: $BUILD_NUMBER%0A%0A<b>👨🏻‍💻COMMITTER</b>: $COMMITTER%0A%0A<b>✍MESSAGE</b>: $COMMIT_MSG%0A%0A<b>📅DATETIME</b>: $DATE $TIME⏰%0A%0A<u>LINK TO SEE COMMIT</u>: https://github.com/Mony-Ratanak/DevOps/commit/$COMMIT_LINK"
+MESSAGE="<b>🚀DEPLOYMENT STATUS</b>: $1%0A%0A<b>🔁VERSION</b>: $BUILD_NUMBER%0A%0A<b>👨🏻‍💻COMMITTER</b>: $COMMITTER%0A%0A<b>✍MESSAGE</b>: $COMMIT_MSG%0A%0A<b>📅DATETIME</b>: $DATE $TIME⏰%0A%0A<u>LINK TO SEE COMMIT</u>: https://github.com/SereyvicheaSaro/Jenkins/$COMMIT_LINK"
 
 # Send Message to Telegram
 curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d chat_id=${CHAT_ID} -d text="${MESSAGE}" -d parse_mode="HTML"
