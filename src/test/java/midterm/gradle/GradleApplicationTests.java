90b0bd9a063ef29eb86ec5d04552b87aca5b1717
Vichea <104241903+CheaTXT@users.noreply.github.com>
2024-03-29 16:04:21 +0700

    first commit

---
+++ src/test/java/midterm/gradle/GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package midterm.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
